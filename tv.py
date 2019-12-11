1ec0175d00d597211a7b7b21068d87f3a44c965c
justincong <justincong324@gmail.com>
2019-12-11 13:56:55 -0500

    fds

--- tv.py
+++ tv.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 import pygame as pg
-import keyboard # will be removed with button
+from pathlib import Path
+import keyboard
 
 # global variables
 DELAY = 2
@@ -32,70 +33,81 @@ kendall_succession = mixer.Sound("audio/kendall_succession.wav")
 audio = (dwight_fire_drill, kendall_succession)
 
 # visuals
-video_agni_kai = cv2.VideoCapture('video/agnikai.mp4')
-video_fire_drill = cv2.VideoCapture('video/fire_drill.mp4')
-video_kendall_succession = cv2.VideoCapture('video/kendall_succession.mp4')
+# video_agni_kai = cv2.VideoCapture('video/agnikai.mp4')
+# video_fire_drill = cv2.VideoCapture('video/fire_drill.mp4')
+# video_kendall_succession = cv2.VideoCapture('video/kendall_succession.mp4')
+video_agni_kai = Path('video/agnikai.mp4')
+video_fire_drill = Path('video/fire_drill.mp4')
+video_kendall_succession = Path('video/kendall_succession.mp4')
 
 videos = (video_agni_kai, video_fire_drill, video_kendall_succession)
-delays = (2, 1, 1)
+# delays = (2, 1, 1)
+
+
+player = OMXPlayer(video_agni_kai)
+
+while True:
+
+    if keyboard.isPressed('a'):
+        player = OMXPlayer(video_fire_drill)
 
 # Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
-cap = video_agni_kai
-window_name = "window"
-
-# Check if camera opened successfully
-if (cap.isOpened()== False):
-  print("Error opening video stream or file")
-
-# Read until video is completed
-
-# Full screen on the video
-cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
-cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
-channel0.play(office_theme, -1)
-while(cap.isOpened()):
-  # Capture frame-by-frame
-    ret, frame = cap.read()
-    if ret == True:
-
-        # Display the resulting frame
-        cv2.imshow(window_name,frame)
-
-        # Press Q on keyboard to  exit
-
-        if cv2.waitKey(DELAY) & 0xFF == ord('q'):
-            break
-        elif cv2.waitKey(DELAY) & 0xFF == ord('a'):
-            VID_ITER = (VID_ITER + 1) % VID_COUNT
-            cap = videos[VID_ITER]
-            DELAY = delays[VID_ITER]
-        elif cv2.waitKey(DELAY) & 0xFF == ord('z'):
-            MUSIC_ITER = (MUSIC_ITER + 1) % MUSIC_COUNT
-            channel0.play(music[MUSIC_ITER], -1)
-        elif cv2.waitKey(DELAY) & 0xFF == ord('w'):
-            pass
-
-        # if cv2.waitKey(DELAY) & 0xFF == ord('q'):
-        #     break
-        # elif cv2.waitKey(DELAY) & 0xFF == ord('a'):
-        #     VID_ITER = (VID_ITER + 1) % VID_COUNT
-        #     cap = videos[VID_ITER]
-        #     DELAY = delays[VID_ITER]
-        # elif cv2.waitKey(DELAY) & 0xFF == ord('z'):
-        #     MUSIC_ITER = (MUSIC_ITER + 1) % MUSIC_COUNT
-        #     channel0.play(music[MUSIC_ITER], -1)
-        # elif cv2.waitKey(DELAY) & 0xFF == ord('w'):
-        #     pass
-
-
-  # Break the loop
-    else:
-        break
-
-# When everything done, release the video capture object
-cap.release()
-
-# Closes all the frames
-cv2.destroyAllWindows()
+# cap = video_agni_kai
+# window_name = "window"
+#
+# # Check if camera opened successfully
+# if (cap.isOpened()== False):
+#   print("Error opening video stream or file")
+#
+# # Read until video is completed
+#
+# # Full screen on the video
+# cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+# cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+#
+# channel0.play(office_theme, -1)
+# while(cap.isOpened()):
+#   # Capture frame-by-frame
+#     ret, frame = cap.read()
+#     if ret == True:
+#
+#         # Display the resulting frame
+#         cv2.imshow(window_name,frame)
+#
+#         # Press Q on keyboard to  exit
+#
+#         if cv2.waitKey(DELAY) & 0xFF == ord('q'):
+#             break
+#         elif cv2.waitKey(DELAY) & 0xFF == ord('a'):
+#             VID_ITER = (VID_ITER + 1) % VID_COUNT
+#             cap = videos[VID_ITER]
+#             DELAY = delays[VID_ITER]
+#         elif cv2.waitKey(DELAY) & 0xFF == ord('z'):
+#             MUSIC_ITER = (MUSIC_ITER + 1) % MUSIC_COUNT
+#             channel0.play(music[MUSIC_ITER], -1)
+#         elif cv2.waitKey(DELAY) & 0xFF == ord('w'):
+#             pass
+#
+#         # if cv2.waitKey(DELAY) & 0xFF == ord('q'):
+#         #     break
+#         # elif cv2.waitKey(DELAY) & 0xFF == ord('a'):
+#         #     VID_ITER = (VID_ITER + 1) % VID_COUNT
+#         #     cap = videos[VID_ITER]
+#         #     DELAY = delays[VID_ITER]
+#         # elif cv2.waitKey(DELAY) & 0xFF == ord('z'):
+#         #     MUSIC_ITER = (MUSIC_ITER + 1) % MUSIC_COUNT
+#         #     channel0.play(music[MUSIC_ITER], -1)
+#         # elif cv2.waitKey(DELAY) & 0xFF == ord('w'):
+#         #     pass
+#
+#
+#   # Break the loop
+#     else:
+#         break
+#
+# # When everything done, release the video capture object
+# cap.release()
+#
+# # Closes all the frames
+# cv2.destroyAllWindows()
